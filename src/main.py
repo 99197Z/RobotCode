4096fd5cee45e88b8ed3d8829a4197f9b19c7ef0
Badgeminer 99197z <benh37@educbe.ca>
2023-11-06 16:17:55 -0700

    make decorators( @driverNeeded and @autopilotOnly) part of State class

--- src/main.py
+++ src/main.py
@@ -8,10 +8,10 @@ brain=Brain()
 # Robot configuration code
 controller_1 = Controller(PRIMARY)
 motor_1_motor_a = Motor(Ports.PORT1, GearSetting.RATIO_18_1, False)
-motor_1_motor_b = Motor(Ports.PORT2, GearSetting.RATIO_18_1, False)
+motor_1_motor_b = Motor(Ports.PORT11, GearSetting.RATIO_18_1, False)
 motor_1 = MotorGroup(motor_1_motor_a, motor_1_motor_b)
-motor_2_motor_a = Motor(Ports.PORT11, GearSetting.RATIO_18_1, True)
-motor_2_motor_b = Motor(Ports.PORT12, GearSetting.RATIO_18_1, True)
+motor_2_motor_a = Motor(Ports.PORT7, GearSetting.RATIO_18_1, True)
+motor_2_motor_b = Motor(Ports.PORT20, GearSetting.RATIO_18_1, True)
 motor_2 = MotorGroup(motor_2_motor_a, motor_2_motor_b)
 
 
@@ -26,88 +26,14 @@ wait(30, MSEC)
 #endregion VEXcode Generated Robot Configuration
 autopilot = False
 
-def driverNeeded(func):
-    def wrapper(*args, **kwargs):
-        if not autopilot:
-            func(*args, **kwargs)
-        
-        #func(*args, **kwargs)
-    return wrapper
 
-def autopilotOnly(func):
-    def wrapper(*args, **kwargs):
-        if autopilot:
-            func(*args, **kwargs)
-        #func(*args, **kwargs)
-    return wrapper
-
-#max speeds
-speed_mult = 0.5
-steer_speed_mult = 0.5
-gas_speed_mult = 1
-autopilot_max_speed = 50 
-driver_pilot_max_speed = 100 
-
-
-
-def clamp(n, min, max): 
-    if n < min: 
-        return min
-    elif n > max: 
-        return max
-    else: 
-        return n 
-
-class speedControlls:
-    def __init__(self,mx):
-        self.diff = 0
-        self.speed = 0
-        self.max_speed = mx
-    def calcSpeed(self,inv):
-        if inv:
-            spdDiff = -self.diff
-        else:
-            spdDiff = self.diff
-        return clamp(self.speed - spdDiff,-self.max_speed,self.max_speed)*speed_mult
-
-    def calcMotors(self):
-        
-        
-        motor_1.set_velocity(self.calcSpeed(False), PERCENT)
-        motor_2.set_velocity(self.calcSpeed(True), PERCENT)
-
-    @driverNeeded
-    def mspeed(self):
-        pos = controller_1.axis3.position()
-        brain.screen.set_cursor(1,1)
-        brain.screen.clear_screen()
-        brain.screen.print(str(pos))
-        self.speed = pos
-        self.calcMotors()
-
-    @driverNeeded
-    def dspeed(self):
-        pos = controller_1.axis4.position()
-        self.diff = pos
-        self.calcMotors()
-    
-    @autopilotOnly
-    def driveSequence(self):
-        pass
-
-
-
-
-
-
-
-speed = speedControlls(driver_pilot_max_speed)
 
 class modes:
     stop = 0
     ap = 1
     mode1 = 2
     mode2 = 3
+
 
 class State:
     def __init__(self) -> None:
@@ -127,6 +53,88 @@ class State:
             "R1": False,
             "R2": False
         }
+
+    def driverNeeded(self,func):
+        def wrapper(*args, **kwargs):
+            if self.mode != modes.stop and self.mode != modes.ap:
+                func(*args, **kwargs)
+            
+            #func(*args, **kwargs)
+        return wrapper
+
+    def autopilotOnly(self,func):
+        def wrapper(*args, **kwargs):
+            if self.mode == modes.ap:
+                func(*args, **kwargs)
+            #func(*args, **kwargs)
+        return wrapper
+
+
+
+
+#max speeds
+speed_mult = 0.5
+steer_speed_mult = 0.5
+gas_speed_mult = 1
+autopilot_max_speed = 50 
+driver_pilot_max_speed = 100 
+
+
+
+def clamp(n, min, max): 
+    if n < min: 
+        return min
+    elif n > max: 
+        return max
+    else: 
+        return n 
+
+state = State()
+class speedControlls:
+    def __init__(self,mx):
+        self.diff = 0
+        self.speed = 0
+        self.max_speed = mx
+        
+    def calcSpeed(self,inv):
+        if inv:
+            spdDiff = -self.diff
+        else:
+            spdDiff = self.diff
+        return clamp(self.speed - spdDiff,-self.max_speed,self.max_speed)*speed_mult
+
+    def calcMotors(self):
+        
+        
+        motor_1.set_velocity(self.calcSpeed(False), PERCENT)
+        motor_2.set_velocity(self.calcSpeed(True), PERCENT)
+
+    @state.driverNeeded
+    def mspeed(self):
+        pos = controller_1.axis3.position()
+        brain.screen.set_cursor(1,1)
+        brain.screen.clear_screen()
+        brain.screen.print(str(pos))
+        self.speed = pos
+        self.calcMotors()
+
+    @state.driverNeeded
+    def dspeed(self):
+        pos = controller_1.axis4.position()
+        self.diff = pos
+        self.calcMotors()
+    
+    @state.autopilotOnly
+    def driveSequence(self):
+        pass
+
+speed = speedControlls(driver_pilot_max_speed)
+
+
+
+
+
+
 
 
 # Register event with a callback function.
