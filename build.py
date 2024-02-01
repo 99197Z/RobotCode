f05ef0689098e5c0c3af37ace34a61dd45cc6e93
Badgeminer <badgedog123@gmail.com>
2024-01-31 17:30:26 -0700

    use homepath

--- build.py
+++ build.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
-result = subprocess.run([r"C:\Users\38302008\Downloads\PortableGit\bin\git.exe", 'rev-parse',"--short","HEAD"], stdout=subprocess.PIPE)
+result = subprocess.run([os.environ["HOMEPATH"]+r"\Downloads\PortableGit\bin\git.exe", 'rev-parse',"--short","HEAD"], stdout=subprocess.PIPE)
 with open("src\\main.py","r") as f:
     with open("src\\out.py","w") as F:
         t = f.read().replace('CODE_VER = "DEV"',f"CODE_VER = {result.stdout}")
