705e4a0400d9c8e3903da1229b207f4cb943f488
nissak1s <kasin.schuett@gmail.com>
2024-02-03 17:58:38 -0500

    show final length

--- build.py
+++ build.py
@@ -7,6 +7,7 @@ with open("src\\main.py","r") as f:
     with open("src\\out.py","w") as F:
         print(f"CODE VER: {result.stdout.decode().removesuffix("\n")}")
         t = f.read().replace('CODE_VER = "DEV"',f"CODE_VER = '{result.stdout.decode().removesuffix("\n")}'")
+        print(f"Final Length: {str(len(t)):>7}")
 
         print("Writing...")
         F.write(t)
