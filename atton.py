4b4527d94cd5e11cf8ea705879743c1a0f44fa15
nissak1s <kasin.schuett@gmail.com>
2024-02-03 20:34:01 -0500

    straight in then backn out

--- atton.py
+++ atton.py
@@ -1,15 +1,15 @@
 Adrive(100,0)
-wait(1300)
+wait(1500)
 
 
-Adrive(100,-100)
-wait(1000)
+#Adrive(100,-100)
+#wait(1000)
 
 #Adrive(-100,-100)
 #wait(1000)
 #
-#Adrive(-100,0)
-#wait(1000)
+Adrive(-100,0)
+wait(1500)
 
 #Adrive(-100,0)
 #wait(300)
